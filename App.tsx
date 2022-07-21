620fa1224c33c80afa1b65c032e7dd86da39d81f
Andre Rousselet <andre098@hotmail.com>
2022-07-21 12:34:00 -0300

    add custom assets and style

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import  { NativeBaseProvider } from 'native-base';
+import { THEME } from './src/styles/theme'
 import SignIn from './src/pages/SignIn';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <SignIn />
     </NativeBaseProvider>
   );
