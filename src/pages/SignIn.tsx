04aa1e7755b0cbc1c4329773d75a42bb5d7e29ff
Andre Rousselet <andre098@hotmail.com>
2022-07-21 14:49:31 -0300

    add states to store username and password with useState from react

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 import Logo from '../assets/logo_primary.svg';
@@ -6,7 +6,10 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 function SignIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
@@ -17,12 +20,14 @@ function SignIn() {
         placeholder="E-mail"
         mb={4}
         InputLeftElement={ <Icon as={ <Envelope color={ colors.gray[300] } /> } ml={4} /> }
+        onChangeText={setUsername}
       />
       <Input
         placeholder="Senha"
         mb={8}
         InputLeftElement={ <Icon as={ <Key color={ colors.gray[300] } /> } ml={4} /> }
         secureTextEntry
+        onChangeText={setPassword}
       />
       <Button title="Entrar" w="full"/>
     </VStack>
