afcf398882ee9b2fb6ce3fdf67f753982ae68c8f
Andre Rousselet <andre098@hotmail.com>
2022-07-21 12:55:57 -0300

    feat Loading component and StatusBar props

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import  { NativeBaseProvider } from 'native-base';
+import  { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import SignIn from './src/pages/SignIn';
+import Loading from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 };
