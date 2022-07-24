bc3b8ab149bd1c17fb2aef58af59648c08c61173
Andre Rousselet <andre098@hotmail.com>
2022-07-24 15:42:03 -0300

    add Routes to be rendered when not Loading

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,7 +1,7 @@
 import { VStack } from 'native-base';
 import Header from '../components/Header';
 
-export function Details() {
+export default function Details() {
   return (
     <VStack flex={1} bg="gray.700">
       <Header title="solicitação" />
