6b1c350b9d036576bc9ccc0e721660d124d71fd2
Andre Rousselet <andre098@hotmail.com>
2022-07-24 18:35:51 -0300

    add signin error treatment/messages

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -18,6 +18,22 @@ function SignIn() {
       return Alert.alert('Entrar', 'Informe e-mail e senha.');
     }
     setisLoading(true);
+    auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      console.log(error);
+      setisLoading(false);
+      if (error.code === 'auth/invalid-email'){
+        return Alert.alert('Entrar', 'E-mail inválido.')
+      }
+      if (error.code === 'auth/wrong-password'){
+        return Alert.alert('Entrar', 'E-mail ou senha inválidos.')
+      }
+      if (error.code === 'auth/user-not-found'){
+        return Alert.alert('Entrar', 'E-mail ou senha inválidos.')
+      }
+      return Alert.alert('Entrar', 'Não foi possível acessar.')
+    })
   }
 
   return (
